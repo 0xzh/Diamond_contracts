96a858f8368ba0540f13cf4b003d0ed5680d8f9b
0xzh <115005794+0xzh@users.noreply.github.com>
2022-11-01 12:47:04 +0000

    Create FuncDiamond.sol

---
+++ FuncDiamond.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity 0.8.15;
+
+import "@solidstate/contracts/proxy/diamond/SolidStateDiamond.sol";
+
+
+contract FuncDiamond is SolidStateDiamond {
+
+
+
+
+}
